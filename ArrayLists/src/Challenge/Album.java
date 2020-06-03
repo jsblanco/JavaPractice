2048ba3a4bac012a259aa67be10af9f16a4b7913
Jorge Sánchez Blanco <jorgesanchezblanco1989@gmail.com>
2020-06-03 02:22:12 +0200

    Moved playlist functs to playlist class (they made no sense in Main)

--- ArrayLists/src/Challenge/Album.java
+++ ArrayLists/src/Challenge/Album.java
@@ -1,36 +1,54 @@
 package Challenge;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 
 public class Album {
-    private LinkedList<Song> tracklist;
+    private ArrayList<Song> tracklist;
 
 
     public Album() {
-        this.tracklist = new LinkedList<Song>();
+        this.tracklist = new ArrayList<Song>();
     }
 
-    public LinkedList<Song> getTracklist() {
+    public ArrayList<Song> getTracklist() {
         return tracklist;
     }
 
     public Song getTrack(int index) {
-        return tracklist.get(index);
+        if (tracklist.size() >0 && (index >=0 && index < tracklist.size())) {
+            return tracklist.get(index);
+        }
+        System.out.println("This album does not have a track number "+index);
+        return null;
     }
 
-    public void addSong(Song song){
-        tracklist.add(song);
+    private boolean isSongAlreadyInTheAlbum(String songToAdd) {
+        for (Song songInAlbum : this.tracklist) {
+            if (songInAlbum.getTitle().equals((songToAdd))) {
+                return true;
+            }
+        }
+        return false;
     }
 
-    public void addSong(int index, Song song){
+    public void addSong(Song song) {
+        String songTitle = song.getTitle();
+        if (!isSongAlreadyInTheAlbum(songTitle)) {
+            tracklist.add(song);
+        } else {
+            System.out.println("Song " + songTitle + " is already in the album");
+        }
+    }
+
+    public void addSong(int index, Song song) {
         tracklist.add(index, song);
     }
 
-    public void removeSong(Song song){
+    public void removeSong(Song song) {
         tracklist.remove(song);
     }
 
-    public void removeSong(int index){
+    public void removeSong(int index) {
         tracklist.remove(index);
     }
 }
--- ArrayLists/src/Challenge/Main.java
+++ ArrayLists/src/Challenge/Main.java
@@ -1,8 +1,6 @@
 package Challenge;
 
 import java.util.ArrayList;
-import java.util.ListIterator;
-import java.util.Scanner;
 
 public class Main {
 
@@ -24,7 +22,8 @@ public class Main {
         albums.add(new Album());
         albums.add(new Album());
         albums.get(0).addSong(new Song("Light up the night", 3.42));
-        albums.get(0).addSong(new Song("The hounds", 4.35));
+        albums.get(0).addSong(new Song("Born to last", 4.35));
+        albums.get(0).addSong(new Song("Born to last", 4.35));
         albums.get(0).addSong(new Song("Hold back the night", 6.21));
         albums.get(1).addSong(new Song("My name is Dark", 6.23));
         albums.get(1).addSong(new Song("Oblivion", 4.12));
@@ -33,118 +32,18 @@ public class Main {
         albums.get(2).addSong(new Song("Justice", 3.28));
         albums.get(2).addSong(new Song("24h Cinderella", 2.52));
 
-        addSongToPlaylist(playlist, albums, 2,0);
-        addSongToPlaylist(playlist, albums, 0,2);
-        addSongToPlaylist(playlist, albums, 1,2);
-        addSongToPlaylist(playlist, albums, 1,1);
-        addSongToPlaylist(playlist, albums, 0,0);
-        addSongToPlaylist(playlist, albums, 2,1);
-        addSongToPlaylist(playlist, albums, 9,9);
+        playlist.addSong( albums.get(2),0);
+        playlist.addSong( albums.get(0),2);
+        playlist.addSong(albums.get(1),0);
+        playlist.addSong(albums.get(1),1);
+        playlist.addSong( albums.get(0),0);
+        playlist.addSong(albums.get(0),1);
+        playlist.addSong(albums.get(1),9);
 
         playlist.printPlaylist();
-        playPlaylist(playlist);
+        playlist.play();
 
     }
 
-    private static void addSongToPlaylist(Playlist playlist, ArrayList albums, int album, int song){
-        if (album>-1&& albums.size()>album) {
-            Album thisAlbum = (Album) albums.get(album);
-            if (song>-1 && thisAlbum.getTracklist().size()> song)
-            playlist.addSong(thisAlbum, song);
-        }
-    }
-
-    private static void playPlaylist(Playlist playlist) {
-        Scanner scanner = new Scanner(System.in);
-        ListIterator<Song> iterator = playlist.listIterator();
-        boolean forward = true;
-        boolean end = false;
-
-        if (iterator.hasNext()) {
-            showMenu(playlist);
-            System.out.println("Now playing: " + playlist.getSong(iterator.nextIndex()).getTitle());
-            iterator.next();
-        } else {
-            System.out.println("Your playlist is empty, please add some songs to your playlist first.");
-            end = true;
-        }
-
-        while (!end) {
-            int action = scanner.nextInt();
-            switch (action) {
-                case 0:
-                    System.out.println("Stopping playlist");
-                    end = true;
-                    break;
-                case 1:
-                    if (forward) {
-                        if (iterator.hasPrevious()) {
-                            iterator.previous();
-                        }
-                        forward = false;
-                    }
-                    if (iterator.hasPrevious()) {
-                        System.out.println("Playing the previous song: " +  playlist.getSong(iterator.previousIndex()).getTitle());
-                        iterator.previous();
-                    } else {
-                        System.out.println("You are at the start of your playlist");
-                    }
-                    break;
-                case 2:
-                    if (!forward){
-                        if (iterator.hasNext()) {
-                            System.out.println("Playing the next song: " +  playlist.getSong(iterator.nextIndex()).getTitle());
-                        } else {
-                            System.out.println("You have reached the end of your playlist");
-                        }
-                    } else {
-                    if (iterator.hasPrevious()) {
-                        System.out.println("Restarting the song: " +  playlist.getSong(iterator.previousIndex()).getTitle());
-                    } else {
-                        System.out.println("You are at the start of your playlist");
-                    }}
-                    break;
-                case 3:
-                    if (!forward) {
-                        if (iterator.hasNext()) {
-                            iterator.next();
-                        }
-                        forward = true;
-                    }
-                    if (iterator.hasNext()) {
-                        System.out.println("Playing the next song: " +  playlist.getSong(iterator.nextIndex()).getTitle());
-                        iterator.next();
-                    } else {
-                        System.out.println("You have reached the end of your playlist");
-                    }
-                    break;
-                case 4:
-                    System.out.println("Removed song from the playlist");
-                    iterator.remove();
-                    break;
-                case 5:
-                    playlist.printPlaylist();
-                    break;
-                case 6:
-                    showMenu(playlist);
-                    break;
-            }
-        }
-
-
-    }
-
-
-    private static void showMenu(Playlist list) {
-        System.out.println("Welcome to your playlist. You currently have "+list.size() + " songs queued.");
-        System.out.println("Please type what you wish to do:");
-        System.out.println("0- Stop playlist");
-        System.out.println("1- Play the previous song");
-        System.out.println("2- Replay the song");
-        System.out.println("3- Play the next song");
-        System.out.println("4- Remove song from the playlist");
-        System.out.println("5- Show the whole playlist");
-        System.out.println("6- Show this menu again");
-    }
 
 }
