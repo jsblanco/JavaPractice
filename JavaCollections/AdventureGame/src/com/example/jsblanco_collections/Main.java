8f241688676056bd43c7af4f12fd2c67ec50c549
Jorge Sánchez Blanco <jorgesanchezblanco1989@gmail.com>
2021-09-10 14:49:25 +0200

    Completed initial Adventure challenge

--- JavaCollections/AdventureGame/src/com/example/jsblanco_collections/Main.java
+++ JavaCollections/AdventureGame/src/com/example/jsblanco_collections/Main.java
@@ -6,35 +6,12 @@ import java.util.Scanner;
 
 public class Main {
     private static final Map<Integer, Location> locations = new HashMap<Integer, Location>();
+    private static final Map<String, String> vocabulary = new HashMap<String, String>();
     private static int loc;
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        locations.put(0, new Location(0, "Te vas de Kamurocho por el arco de Tenkaichi St."));
-        locations.put(1, new Location(1, "Estás en la azotea de la Millenium Tower"));
-        locations.put(2, new Location(2, "Estás en el Sega de Theatre Sq. jugando a MesuKing"));
-        locations.put(3, new Location(3, "Estás en Earth Angel tomándote un copazo"));
-        locations.put(4, new Location(4, "Estás en Don Quijote, cantando la musiquilla"));
-        locations.put(5, new Location(5, "Estás en Kamurocho Hills, en la planta baja"));
-
-        locations.get(1).addExit("N", 5);
-        locations.get(1).addExit("S", 4);
-        locations.get(1).addExit("E", 3);
-        locations.get(1).addExit("W", 2);
-        // locations.get(1).addExit("Q", 0);
-        locations.get(2).addExit("N", 5);
-        //locations.get(2).addExit("Q", 0);
-        locations.get(3).addExit("W", 1);
-        //locations.get(3).addExit("Q", 0);
-        locations.get(4).addExit("N", 1);
-        locations.get(4).addExit("W", 2);
-        //locations.get(4).addExit("Q", 0);
-        locations.get(5).addExit("S", 1);
-        locations.get(5).addExit("W", 2);
-        //locations.get(5).addExit("Q", 0);
-
-        //Quitamos las Q para ponerlas en el constructor de la clase, ahorrando tiempo y código
+        populateLocations();
+        populateVocabulary();
 
         initialWelcome();
 
@@ -45,7 +22,7 @@ public class Main {
             Map<String, Integer> exits = locations.get(loc).getExits();
             stateExits(exits);
 
-            String direction = scanner.nextLine().toUpperCase();
+            String direction = getDirection();
             changeLocation(exits, direction);
         }
     }
@@ -74,5 +51,60 @@ public class Main {
             System.out.println("No puedes ir ahí.");
     }
 
+    private static String getDirection() {
+        Scanner scanner = new Scanner(System.in);
 
+/*
+        String[] input = scanner.nextLine().toUpperCase().split(" ");
+        char direction = input[input.length - 1].charAt(0);
+        return String.valueOf(direction);
+*/
+
+        String direction = scanner.nextLine().toUpperCase();
+        if (direction.length() > 1) {
+            String[] inputWords = direction.split(" ");
+            for (String word : inputWords) {
+                if (vocabulary.containsKey(word)) {
+                    direction = vocabulary.get(word);
+                    break;
+                }
+            }
+        }
+        return direction;
+    }
+
+    private static void populateLocations() {
+        locations.put(0, new Location(0, "Te vas de Kamurocho por el arco de Tenkaichi St."));
+        locations.put(1, new Location(1, "Estás en la azotea de la Millenium Tower"));
+        locations.put(2, new Location(2, "Estás en el Sega de Theatre Sq. jugando a MesuKing"));
+        locations.put(3, new Location(3, "Estás en Earth Angel tomándote un copazo"));
+        locations.put(4, new Location(4, "Estás en Don Quijote, cantando la musiquilla"));
+        locations.put(5, new Location(5, "Estás en Kamurocho Hills, en la planta baja"));
+
+        locations.get(1).addExit("N", 5);
+        locations.get(1).addExit("S", 4);
+        locations.get(1).addExit("E", 3);
+        locations.get(1).addExit("W", 2);
+        // locations.get(1).addExit("Q", 0);
+        locations.get(2).addExit("N", 5);
+        //locations.get(2).addExit("Q", 0);
+        locations.get(3).addExit("W", 1);
+        //locations.get(3).addExit("Q", 0);
+        locations.get(4).addExit("N", 1);
+        locations.get(4).addExit("W", 2);
+        //locations.get(4).addExit("Q", 0);
+        locations.get(5).addExit("S", 1);
+        locations.get(5).addExit("W", 2);
+        //locations.get(5).addExit("Q", 0);
+
+        //Quitamos las Q para ponerlas en el constructor de la clase, ahorrando tiempo y código
+    }
+
+    private static void populateVocabulary() {
+        vocabulary.put("QUIT", "Q");
+        vocabulary.put("NORTH", "N");
+        vocabulary.put("SOUTH", "S");
+        vocabulary.put("EAST", "E");
+        vocabulary.put("WEST", "W");
+    }
 }
