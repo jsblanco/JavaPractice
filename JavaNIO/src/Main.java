0398f13c0548364f959107a013ef433182d24beb
Jorge Sánchez Blanco <jorgesanchezblanco1989@gmail.com>
2021-10-07 16:11:23 +0200

    Created new project to study NIO in depth

---
+++ JavaNIO/src/Main.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            Path dataPath = FileSystems.getDefault().getPath("data.txt");
+            Files.write(dataPath, "\nLine 3".getBytes(StandardCharsets.UTF_8), StandardOpenOption.APPEND);
+            List<String> lines = Files.readAllLines(dataPath);
+            for (String line : lines)
+                System.out.println(line);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
